1e8387454ee242dff10f91b23658c81f7afc050a
Luis Aroldo Morales Noriega <ingluis07@gmail.com>
2019-02-26 14:47:49 -0600

    Segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HolamundoComponent } from './holamundo/holamundo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HolamundoComponent
   ],
   imports: [
     BrowserModule
